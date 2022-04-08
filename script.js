cda3452624d1076e25e0446edee809c6d58bbb2c
JonRodriguez98 <jonrodriguez4898@gmail.com>
2022-04-08 11:53:15 -0400

    Added button functionality to JS

--- script.js
+++ script.js
@@ -3,29 +3,43 @@ let buttons = document.querySelectorAll(".button")
 let pokeName = document.querySelector(".pokemon")
 let pokeType = document.querySelector(".type")
 let pokeID = document.querySelector(".input")
-let fwdBtn = document.querySelector(".incDec")
+let fwdBtn = document.querySelector(".increase")
 let backBtn = document.querySelector(".decrease")
-let delete = document.querySelector(".delete")
-
-generatePokemon()
-
+let deletes = document.querySelector(".delete")
+let fetchPoke = document.querySelector(".blueCircle2")
 
 
 
 
+buttons.forEach((btn, idx) => {
+  btn.addEventListener('click', (b) =>{
+    let userInput = b.target.innerText;
+    pokeID.append(userInput);
+  })
+});
+
+fwdBtn.addEventListener('click', () => {
+  pokeID.innerHTML++
+})
+
+backBtn.addEventListener('click',() => {
+  pokeID.innerHTML--
+})
+
+deletes.addEventListener('click',() => {
+  pokeID.innerText = ""
+})
+
+fetchPoke.addEventListener('click', () => generatePokemon(pokeID))
 
 
+async function generatePokemon(pokeID) {
+  let id = pokeID.innerText
+  const url = "https://pokeapi.co/api/v2/pokemon/${id}"
 
 
-function generatePokemon() {
-  const config = {
-    headers: {
-      "Accept" : "application/json"
-    }
-
-  }
-
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon",config)
+  const res = await fetch(url)
   const data = await res.json()
-  console.log(data)
+  console.log(data.name)
+
 }

6390e96ca39215250a2edfabc157a5bd66454b73
JonRodriguez98 <jonrodriguez4898@gmail.com>
2022-04-08 11:53:44 -0400

    More JS update

--- script.js
+++ script.js
@@ -35,11 +35,12 @@ fetchPoke.addEventListener('click', () => generatePokemon(pokeID))
 
 async function generatePokemon(pokeID) {
   let id = pokeID.innerText
-  const url = "https://pokeapi.co/api/v2/pokemon/${id}"
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 
 
   const res = await fetch(url)
   const data = await res.json()
   console.log(data.name)
+  
 
 }
